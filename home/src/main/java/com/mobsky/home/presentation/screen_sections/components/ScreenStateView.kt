d82b1ce4494e878fe76626b3aa8e5513cee9a2dc
Bruno Baudel <brunobaudel@gmail.com>
2023-05-10 23:55:18 -0300

    Separa o shimmer para o pacote de utilidades

--- home/src/main/java/com/mobsky/home/presentation/screen_sections/components/ScreenStateView.kt
+++ home/src/main/java/com/mobsky/home/presentation/screen_sections/components/ScreenStateView.kt
@@ -1,10 +1,5 @@
 package com.mobsky.home.presentation.screen_sections.components
 
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,9 +17,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mobsky.home.presentation.util.ScreenState
@@ -44,19 +36,17 @@ fun ScreenStateView(
 
         when (val state = screenState.taskStateScreen) {
             TaskState.Complete -> content?.invoke()
-            is TaskState.Error -> errorScreen(Exception("Test preview error!"))
+            is TaskState.Error -> ErrorScreen(state.exception)
             TaskState.NotStarted,
-            TaskState.InProgress -> load(showShimmer.value)
+            TaskState.InProgress -> LoadScreen(showShimmer.value)
         }
     }
 }
 
 @Composable
-private fun errorScreen(exception: Exception) {
+private fun ErrorScreen(exception: Exception) {
 
-    Column(
-        modifier = Modifier
-    ) {
+    Column {
         Icon(
             imageVector = Icons.Filled.Info,
             modifier = Modifier
@@ -73,7 +63,7 @@ private fun errorScreen(exception: Exception) {
 }
 
 @Composable
-private fun load(showShimmer: Boolean) {
+private fun LoadScreen(showShimmer: Boolean) {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -87,40 +77,7 @@ private fun load(showShimmer: Boolean) {
 @Composable
 fun ScreenStateViewPreview() {
 
-    val screenState =
-        // ScreenState(TaskState.Error(Exception("Test preview error!")))
-        ScreenState(TaskState.InProgress)
+    val screenState = ScreenState(TaskState.InProgress)
 
     ScreenStateView(screenState)
-}
-
-@Composable
-fun shimmerBrush(showShimmer: Boolean = true, targetValue: Float = 1000f): Brush {
-    return if (showShimmer) {
-        val shimmerColors = listOf(
-            Color.LightGray.copy(alpha = 0.6f),
-            Color.LightGray.copy(alpha = 0.2f),
-            Color.LightGray.copy(alpha = 0.6f),
-        )
-
-        val transition = rememberInfiniteTransition()
-        val translateAnimation = transition.animateFloat(
-            initialValue = 0f,
-            targetValue = targetValue,
-            animationSpec = infiniteRepeatable(
-                animation = tween(800), repeatMode = RepeatMode.Reverse
-            )
-        )
-        Brush.linearGradient(
-            colors = shimmerColors,
-            start = Offset.Zero,
-            end = Offset(x = translateAnimation.value, y = translateAnimation.value)
-        )
-    } else {
-        Brush.linearGradient(
-            colors = listOf(Color.Transparent, Color.Transparent),
-            start = Offset.Zero,
-            end = Offset.Zero
-        )
-    }
 }
