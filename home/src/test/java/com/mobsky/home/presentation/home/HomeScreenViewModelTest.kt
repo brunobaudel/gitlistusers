e8d706a17b003ac202e98356246c0413b34872cb
Bruno Baudel <brunobaudel@gmail.com>
2023-05-15 10:35:27 -0300

    Ajusta o redme da aplicacao

--- home/src/test/java/com/mobsky/home/presentation/home/HomeScreenViewModelTest.kt
+++ home/src/test/java/com/mobsky/home/presentation/home/HomeScreenViewModelTest.kt
@@ -1,22 +1,12 @@
 package com.mobsky.home.presentation.home
 
-
-
-import org.junit.Assert.assertThat
 import org.junit.Test
+
 
 class HomeScreenViewModelTest {
 
-//    @get:Rule
-//    val rule = InstantTaskExecutorRule()
-//
-//    private lateinit var viewModel: HomeScreenViewModel
-//
-//    @Mock
-//    private val getUsersUseCase = mock<GetUsersUseCase>()
-
     @Test
-    fun `when view model getUsers get success then sets uiState TaskState to Complete`(){
+    fun simpleTest(){
 
         val result = 1
 
